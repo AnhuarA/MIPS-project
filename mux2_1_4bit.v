fa2a6f335439a774be6c2bb266f9807ff126df7c
AnhuarA <aaabrajan@cpp.edu>
2017-03-06 16:45:39 -0800

    MUX Update

--- mux2_1_4bit.v
+++ mux2_1_4bit.v
@@ -27,9 +27,9 @@ module mux2_1_4bit(
     output [3:0] out
     );
     
-    wire [3:0] aOut, bOut;
-    
-    and andA(aOut, a, ~sel);
-    and andB(bOut, b, sel);
-    or  or1(out, aOut, bOut);
+    mux2_1_1bit mux0(a[0],b[0],sel,out[0]);
+    mux2_1_1bit mux1(a[1],b[1],sel,out[1]);
+    mux2_1_1bit mux2(a[2],b[2],sel,out[2]);
+    mux2_1_1bit mux3(a[3],b[3],sel,out[3]);    
+    mux2_1_1bit mux4(a[4],b[4],sel,out[4]);
 endmodule
