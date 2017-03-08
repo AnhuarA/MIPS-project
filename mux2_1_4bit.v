7aca43a7b7966ef048981f2a1c10a75c9677e43d
AnhuarA <aaabrajan@cpp.edu>
2017-03-08 09:48:55 -0800

    Update mux2_1_4bit.v

--- mux2_1_4bit.v
+++ mux2_1_4bit.v
@@ -23,13 +23,16 @@
 module mux2_1_4bit(
     input [3:0] a,
     input [3:0] b,
+    input en,
     input  sel,
     output [3:0] out
     );
+
+    wire notS;
+    wire [3:0] aOut, bOut;
     
-    mux2_1_1bit mux0(a[0],b[0],sel,out[0]);
-    mux2_1_1bit mux1(a[1],b[1],sel,out[1]);
-    mux2_1_1bit mux2(a[2],b[2],sel,out[2]);
-    mux2_1_1bit mux3(a[3],b[3],sel,out[3]);    
-    mux2_1_1bit mux4(a[4],b[4],sel,out[4]);
+    not notSel(notS, sel);
+    and and0[3:0](aOut, notS, en, a );
+    and and1[3:0](bOut, sel, en, b );
+    or or0[3:0](out, aOut,bOut);
 endmodule
